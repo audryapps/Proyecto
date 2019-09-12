079c1175771ef1201a71b0cacc465772a07992ae
Felipe Vargas <felipevargas_96@hotmail.com>
2019-09-12 14:25:50 -0500

    Login

---
+++ tasking/src/app/logueo/logueo.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {FireDBService} from '../Servicios/fire-db.service'
+
+@Component({
+  selector: 'app-logueo',
+  templateUrl: './logueo.page.html',
+  styleUrls: ['./logueo.page.scss'],
+})
+export class LogueoPage implements OnInit {
+
+  constructor(public fireDB:FireDBService) { }
+
+  ngOnInit() {
+  }
+
+}
+
